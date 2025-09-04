0434ab9e69910dcfbe5ac67f2e60986327133245
suvo <suvodatta72@gamil.com>
2025-09-04 16:44:20 +0600

    initial setup done

---
+++ UI/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app_style">
+      <h1 className="text-4xl">This is Portfolio website</h1>
+      <p className="text-2xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
